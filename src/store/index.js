46c7d3bbd712963963fc8e128e864a5946fb0c92
Tony Jemba <jjembatony@gmail.com>
2022-07-28 07:12:07 +0300

    adding user from vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,29 @@
 import { createStore } from "vuex";
-import data from '@/data.json';
+import data from "@/data.json";
 
 export default createStore({
-    state: data,
-    actions:{
-        createPost(context,post){
-            //setting post.id explicitly(will be automated by database)
-             post.id = 'dummy'+ Math.random()
-            context.commit('setPost',{post});
-            context.commit('appendPostToThread',{postId:post.id,threadId:post.threadId})
-        }
+  state: { ...data, authId: "VXjpr2WHa8Ux4Bnggym8QFLdv5C3" },
+  getters: {
+    authUser: (state) => state.users.find((user) => user.id === state.authId),
+  },
+  actions: {
+    createPost(context, post) {
+      //setting post.id explicitly(will be automated by database)
+      post.id = "dummy" + Math.random();
+      context.commit("setPost", { post });
+      context.commit("appendPostToThread", {
+        postId: post.id,
+        threadId: post.threadId,
+      });
     },
-    mutations:{
-        setPost(state,{post}){
-            state.posts.push(post);
-            
-        },
-        appendPostToThread(state,{postId, threadId}){
-          const thread =   state.thread.find(thread=>thread.id === threadId);
-          thread.posts.push(postId);
-        }
-    }
+  },
+  mutations: {
+    setPost(state, { post }) {
+      state.posts.push(post);
+    },
+    appendPostToThread(state, { postId, threadId }) {
+      const thread = state.thread.find((thread) => thread.id === threadId);
+      thread.posts.push(postId);
+    },
+  },
 });
